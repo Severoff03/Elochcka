02056f6ed7d8bc399202c1d14e52fa9b6e276fe1
_antisocial_squirrel_ <belkinandrej924@gmail.com>
2022-03-30 16:44:01 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void elochcka(int height)
+{
+    int height1 = height;\
+    int num =1;
+
+    while (height != 0)
+    {
+
+        char space = ' ';
+        for (int i = height; i > 0; i--)
+        {
+            cout << ' ';
+        }
+        height--;
+        for (int j = 0; j<num; j++)
+        {
+            cout << '*';
+
+        }
+        num+=2;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int height = 4;
+    elochcka(height);
+    return 0;
+}
